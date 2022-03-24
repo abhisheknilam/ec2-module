4b4384ca1906be9ad14ccfd02b5ae835608590bd
Abhishek Nilam <96540453+abhisheknilam@users.noreply.github.com>
2022-03-24 21:27:30 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "demo_allow_http" {
+  name        = "demo_allow_http"
+  description = "Allow hhtp inbound traffic"
+  vpc_id      = "vpc-096e88d61502d2994"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    //ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    //ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allowed inbound http sg and all outbound sg "
+  }
+}
